f51009b9c1bf7f117a877f6fd114da3fef51c5a8
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-02-13 13:53:20 +0100

    Add profiles

--- src/main/js/LdapConfigurationForm.js
+++ src/main/js/LdapConfigurationForm.js
@@ -1,7 +1,7 @@
 //@flow
 
 import React from "react";
-import {Checkbox, Configuration, DropDown, InputField, Select} from "@scm-manager/ui-components";
+import {Checkbox, Configuration, InputField, Select} from "@scm-manager/ui-components";
 import {translate} from "react-i18next";
 
 type LdapConfiguration = {
@@ -33,7 +33,7 @@ type Props = {
 }
 
 type State = LdapConfiguration & {
-  configurationChanged: boolean
+  activeFields: string[]
 }
 
 class LdapConfigurationForm extends React.Component<Props, State> {
@@ -41,27 +41,43 @@ class LdapConfigurationForm extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      ...props.initialConfiguration
+      ...props.initialConfiguration,
+      activeFields: []
     };
   }
 
-  valueChangeHandler = (value: string, name: string) => {
+  valueChangeHandler = (value: any, name: string) => {
     this.setState({
       [name]: value
     }, () => this.props.onConfigurationChange({...this.state}, true));
   };
 
+  profileChangedHandler = (value: string) => {
+    const profile = this.profiles[value];
+    const fields = Object.keys(profile);
+    this.setState({profile: value, activeFields: fields, ...profile},
+      () => this.props.onConfigurationChange({...this.state}, true))
+  };
+
   render(): React.ReactNode {
+    const {t} = this.props;
+    const profiles = Object.keys(this.profiles);
+
     return (
       <>
-        {this.createDropDown("profile", ["Custom"])}
+        <Select name="profile"
+                label={t("scm-ldap-plugin.form.profile")}
+                helpText={t("scm-ldap-plugin.form.profileHelp")}
+                value={this.state.profile}
+                options={this.createOptions("profile", profiles)}
+                onChange={this.profileChangedHandler}/>
         {this.createInputField("attributeNameId")}
         {this.createInputField("attributeNameFullname")}
         {this.createInputField("attributeNameMail")}
         {this.createInputField("attributeNameGroup")}
         {this.createInputField("baseDn")}
         {this.createInputField("connectionDn")}
-        {this.createInputField("connectionPassword")}
+        {this.createInputField("connectionPassword", "password")}
         {this.createInputField("hostUrl")}
         {this.createInputField("searchFilter")}
         {this.createInputField("searchFilterGroup")}
@@ -76,39 +92,135 @@ class LdapConfigurationForm extends React.Component<Props, State> {
     );
   }
 
-  createDropDown = (name: string, options: string[]) => {
+  createDropDown = (name: string, options: string[], handler = this.valueChangeHandler) => {
     const {t} = this.props;
-    return (<Select name={name}
-                    label={t("scm-ldap-plugin.form." + name)}
-                    helpText={t("scm-ldap-plugin.form." + name + "Help")}
-                    value={options[0]}
-                    options={options.map(value => {
-                      return {
-                        value: value,
-                        label: t("scm-ldap-plugin.form.options." + name + "." + value)
-                      };
-                    })}
-                    onChange={this.valueChangeHandler}/>);
+    return this.ifActive(name, (<Select name={name}
+                                   label={t("scm-ldap-plugin.form." + name)}
+                                   helpText={t("scm-ldap-plugin.form." + name + "Help")}
+                                   value={options[0]}
+                                   options={this.createOptions(name, options)}
+                                   onChange={handler}/>));
   };
 
-  createInputField = (name: string) => {
+  createOptions = (name: string, options: string[]) => {
+    const {t} = this.props;
+    return options.map(value => {
+      return {
+        value: value,
+        label: t("scm-ldap-plugin.form.options." + name + "." + value)
+      };
+    })
+  };
+
+  createInputField = (name: string, type: string = "text") => {
     const {t, readOnly} = this.props;
-    return (<InputField name={name}
-                        label={t("scm-ldap-plugin.form." + name)}
-                        helpText={t("scm-ldap-plugin.form." + name + "Help")}
-                        disabled={readOnly}
-                        value={this.state[name]}
-                        onChange={this.valueChangeHandler}/>);
+    return this.ifActive(name, (<InputField name={name}
+                                       label={t("scm-ldap-plugin.form." + name)}
+                                       helpText={t("scm-ldap-plugin.form." + name + "Help")}
+                                       disabled={readOnly}
+                                       value={this.state[name]}
+                                       type={type}
+                                       onChange={this.valueChangeHandler}/>));
   };
 
   createCheckbox = (name: string) => {
     const {t, readOnly} = this.props;
-    return (<Checkbox name={name}
-                      label={t("scm-ldap-plugin.form." + name)}
-                      helpText={t("scm-ldap-plugin.form." + name + "Help")}
-                      checked={this.state[name]}
-                      disabled={readOnly}
-                      onChange={this.valueChangeHandler}/>);
+    return this.ifActive(name, (<Checkbox name={name}
+                                     label={t("scm-ldap-plugin.form." + name)}
+                                     helpText={t("scm-ldap-plugin.form." + name + "Help")}
+                                     checked={this.state[name]}
+                                     disabled={readOnly}
+                                     onChange={this.valueChangeHandler}/>));
+  };
+
+  ifActive = (name: string, component: any) => {
+    console.log("active fields:", this.state.activeFields);
+    if (this.state.activeFields.length === 0 || this.state.activeFields.includes(name)) {
+      return component;
+    } else {
+      return null;
+    }
+  };
+
+  profiles = {
+    'AD': {
+      attributeNameId: 'sAMAccountName',
+      attributeNameFullname: 'cn',
+      attributeNameMail: 'mail',
+      attributeNameGroup: 'memberOf',
+      searchFilter: '(&(objectClass=Person)(sAMAccountName={0}))',
+      searchFilterGroup: '(&(objectClass=group)(member={0}))',
+      searchScope: 'sub',
+      unitPeople: '',
+      unitGroups: '',
+      referralStrategy: 'FOLLOW'
+    },
+    'Apache': {
+      attributeNameId: 'cn',
+      attributeNameFullname: 'displayName',
+      attributeNameMail: 'mail',
+      attributeNameGroup: 'memberOf',
+      searchFilter: '(&(objectClass=inetOrgPerson)(cn={0}))',
+      searchFilterGroup: '(&(objectClass=groupOfUniqueNames)(uniqueMember={0}))',
+      searchScope: 'sub',
+      unitPeople: 'ou=People',
+      unitGroups: 'ou=Groups',
+      enableNestedADGroups: 'false',
+      referralStrategy: 'FOLLOW'
+    },
+    'OpenDJ': {
+      attributeNameId: 'uid',
+      attributeNameFullname: 'cn',
+      attributeNameMail: 'mail',
+      attributeNameGroup: 'memberOf',
+      searchFilter: '(&(objectClass=inetOrgPerson)(uid={0}))',
+      searchFilterGroup: '(&(objectClass=groupOfUniqueNames)(uniqueMember={0}))',
+      searchScope: 'sub',
+      unitPeople: 'ou=People',
+      unitGroups: 'ou=Groups',
+      referralStrategy: 'FOLLOW',
+      enableNestedADGroups: 'false'
+    },
+    'OpenLDAP': {
+      attributeNameId: 'uid',
+      attributeNameFullname: 'cn',
+      attributeNameMail: 'mail',
+      attributeNameGroup: 'memberOf',
+      searchFilter: '(&(objectClass=inetOrgPerson)(uid={0}))',
+      searchFilterGroup: '(&(objectClass=groupOfUniqueNames)(uniqueMember={0}))',
+      searchScope: 'sub',
+      unitPeople: 'ou=People',
+      unitGroups: 'ou=Groups',
+      referralStrategy: 'FOLLOW',
+      enableNestedADGroups: 'false'
+    },
+    'posix': {
+      attributeNameId: 'uid',
+      attributeNameFullname: 'cn',
+      attributeNameMail: 'mail',
+      attributeNameGroup: 'memberOf',
+      searchFilter: '(&(objectClass=posixAccount)(uid={0}))',
+      searchFilterGroup: '(&(objectClass=posixGroup)(memberUid={1}))',
+      searchScope: 'sub',
+      unitPeople: 'ou=People',
+      unitGroups: 'ou=Groups',
+      referralStrategy: 'FOLLOW',
+      enableNestedADGroups: 'false'
+    },
+    'sun': {
+      attributeNameId: 'uid',
+      attributeNameFullname: 'cn',
+      attributeNameMail: 'mail',
+      attributeNameGroup: 'memberOf',
+      searchFilter: '(&(objectClass=inetOrgPerson)(uid={0}))',
+      searchFilterGroup: '(&(objectClass=groupOfUniqueNames)(uniqueMember={0}))',
+      searchScope: 'sub',
+      unitPeople: 'ou=People',
+      unitGroups: 'ou=Groups',
+      referralStrategy: 'FOLLOW',
+      enableNestedADGroups: 'false'
+    },
+    'Custom': {}
   };
 }
 
